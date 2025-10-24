9bbf7a844ced4ac829af2ac323072a9686dc2d3b
mohamadsahim2004-sahim <mohamadsahim2004@gmail.com>
2025-10-24 09:55:10 +0530

    sahim

---
+++ voting_system/election.h
@@ -0,0 +1,48 @@
+// election.h
+#ifndef ELECTION_H
+#define ELECTION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_CANDIDATES 5
+#define MAX_DISTRICTS 5
+#define MAX_PARTIES 3
+#define MAX_NAME_LENGTH 20
+#define PASSWORD "official@1234"
+#define CURRENT_YEAR 2025
+
+// --- Data Structures ---
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    char nic[20];
+    int number;
+    int votes;
+} Candidate;
+
+typedef struct {
+    Candidate parties[MAX_PARTIES][MAX_CANDIDATES];
+} District;
+
+// --- Global Variables ---
+extern District districts[MAX_DISTRICTS];
+extern const char *district_names[MAX_DISTRICTS];
+extern const char party_names[MAX_PARTIES];
+
+// --- Shared Functions ---
+int isValidNIC(const char *nic);
+int getBirthYearFromNIC(const char *nic);
+int isUniqueCandidateNumber(int number);
+int getDistrictIndex(int choice);
+int getPartyIndex(char party);
+int hasVoted(const char *nic);
+int isCandidateRegistered(const char *nic);
+
+// --- Functional Modules ---
+void registerCandidate();
+void vote();
+void showOfficials();
+
+#endif
---
+++ voting_system/main.c
@@ -0,0 +1,26 @@
+// main.c
+#include "election.h"
+
+int main() {
+    int choice;
+    while (1) {
+        printf("\n--------------------------------------------------\n");
+        printf("|      Welcome to the Election Voting System      |\n");
+        printf("--------------------------------------------------\n");
+        printf("1. Voting\n");
+        printf("2. Candidate Register\n");
+        printf("3. Officials\n");
+        printf("4. Exit\n");
+        printf("--------------------------------------------------\n");
+        printf("Enter your choice (1-4): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: vote(); break;
+            case 2: registerCandidate(); break;
+            case 3: showOfficials(); break;
+            case 4: printf("Exiting...\n"); return 0;
+            default: printf("Invalid choice!\n");
+        }
+    }
+}
